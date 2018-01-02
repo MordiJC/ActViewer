8067bf97265489e23546b52a6ae2e0c15e582f0d
Jakub Czapiga <jakub.czapiga@outlook.com>
2018-01-03 00:05:38 +0100

    Searching methods added

--- src/main/java/io/gihub/mordijc/Act.java
+++ src/main/java/io/gihub/mordijc/Act.java
@@ -1,37 +1,30 @@
 package io.gihub.mordijc;
 
 import io.gihub.mordijc.container.ActElement;
+import io.gihub.mordijc.parser.ActParserSection;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
+import static io.gihub.mordijc.parser.ActParserSection.CHAPTER;
+import static io.gihub.mordijc.parser.ActParserSection.SECTION;
 
 public class Act {
-    private final ActElement rootElement;
-    private final Map<String, IndexElement> indexTable;
-
-    public class IndexElement {
-        public final ActElement element;
-        public final Map<String, IndexElement> children;
-
-        IndexElement(ActElement element, Map<String, IndexElement> childrenMap) {
-            this.element = element;
-            this.children = childrenMap;
-        }
-    }
+    public final List<ActElement> articles;
+    private final IndexElement rootElement;
 
     public Act(ActElement actElement) {
-        if(actElement == null || actElement.isEmpty()) {
+        if (actElement == null || actElement.isEmpty()) {
             throw new IllegalArgumentException("ActElement cannot be null or empty");
         }
 
-        this.rootElement = actElement;
-        this.indexTable = mapElement(rootElement);
+        this.rootElement = new IndexElement(actElement, mapElement(actElement);
+        this.articles = getAllArticles(actElement);
     }
 
     private Map<String, IndexElement> mapElement(ActElement actElement) {
         Map<String, IndexElement> result = new HashMap<>();
 
-        for (ActElement child: actElement.getChildrenActElements()) {
+        for (ActElement child : actElement.getChildrenActElements()) {
             result.put(
                     child.strippedIdentifier(),
                     new IndexElement(
@@ -41,6 +34,122 @@ public class Act {
             );
         }
 
-        return  result;
+        return result;
+    }
+
+    private List<ActElement> getAllArticles(ActElement actElement) {
+        List<ActElement> result = new ArrayList<>();
+
+        if(actElement.type == ActParserSection.ARTICLE) {
+            result.add(actElement);
+        } else {
+            for(ActElement element: actElement.getChildrenActElements()) {
+                if(element.type == ActParserSection.ARTICLE) {
+                    result.add(element);
+                } else if(element.type.ordinal() > ActParserSection.ARTICLE.ordinal()) {
+                    result.addAll(
+                            getAllArticles(element)
+                    );
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public ActElement getArticleByIdentifier(String identifier) {
+        for(ActElement element: articles) {
+            if(element.identifier.equalsIgnoreCase(identifier)) {
+                return element;
+            }
+        }
+
+        throw new NoSuchElementException(String.format("Article with identifier `%s` has not been found", identifier));
+    }
+
+    public ActElement getArticleElementByIdentifier(String articleIdentifier, String[] path) {
+        ActElement result = getArticleByIdentifier(articleIdentifier);
+
+        for(String location: path) {
+            ActElement searchResult =
+                    result.getChildrenActElements()
+                            .stream()
+                            .filter(e->e.identifier.equalsIgnoreCase(location))
+                            .findFirst()
+                            .orElse(null);
+
+            if(searchResult == null) {
+                throw new NoSuchElementException(
+                        String.format("Article `%s` does not have element under path: `%s`",
+                                articleIdentifier,
+                                String.join("/", path))
+                );
+            } else {
+                result = searchResult;
+            }
+        }
+
+        return result;
+    }
+
+    public List<ActElement> getArticlesRange(String startArticle, String endArticle) {
+        int startIndex = -1;
+        int endIndex = -1;
+
+        for(int i = 0; i < articles.size(); ++i) {
+            if(articles.get(i).identifier.equalsIgnoreCase(startArticle)) {
+                startIndex = i;
+            } else if(articles.get(i).identifier.equalsIgnoreCase(endArticle)) {
+                endIndex = i;
+            }
+        }
+
+        if(startIndex > endIndex) {
+            throw new IndexOutOfBoundsException("Starting article cannot be greater than ending one.");
+        }
+
+        if(startIndex == -1) {
+            throw new IndexOutOfBoundsException("Starting article not found.");
+        }
+
+        if(endIndex == -1) {
+            throw new IndexOutOfBoundsException("Ending index not found.");
+        }
+
+
+        return articles.subList(startIndex, endIndex);
+    }
+
+    public ActElement getChapterByIdentifier(String identifier) {
+       return findElement(rootElement.element, identifier, CHAPTER);
+    }
+
+    public ActElement getSectionByIdentifier(String identifier) {
+        return findElement(rootElement.element, identifier, SECTION);
+    }
+
+    private ActElement findElement(ActElement actElement, String identifier, ActParserSection type) {
+        ActElement root = rootElement.element;
+
+        for(ActElement element: root.getChildrenActElements()) {
+            if(element.type == type
+                    && element.identifier.equalsIgnoreCase(identifier)) {
+                return element;
+            } else if(element.type.ordinal() > type.ordinal()) {
+                return findElement(element, identifier, type);
+            }
+        }
+
+        throw new NoSuchElementException("Chapter not found.");
+    }
+
+    public class IndexElement {
+        public final ActElement element;
+        public final Map<String, IndexElement> children;
+
+        IndexElement(ActElement element, Map<String, IndexElement> childrenMap) {
+            this.element = element;
+            this.children = childrenMap;
+        }
     }
 }
